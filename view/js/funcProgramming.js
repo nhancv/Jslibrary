a89eb208753ae64fca6ab491cfaa608827c2ceae
Nhan Cao <nhancv1992@gmail.com>
2016-03-20 17:52:54 +0530

    example 1

--- view/js/funcProgramming.js
+++ view/js/funcProgramming.js
@@ -2,10 +2,26 @@
  * Created by nhancao on 3/20/16.
  */
 
-var Person = (function(){
-    console.log("helloworld");
-});
+var Person = function(){
+    //private properties
+    var hair;
+
+    //public method
+    this.getHair=function(){
+        return hair;
+    };
+    this.setHair=function(value){
+        hair = value;
+    };
+
+    Person.prototype.init = function(hair){
+        this.setHair(hair);
+        return this;
+    }
+
+};
 
 
-Person();
+var person = new Person().init("black");
+console.log(person.getHair());
 
